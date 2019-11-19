a30e7c8ebf58be79604d3edbf64165a66d978561
Brad Donakowski <braddonako@gmail.com>
2019-11-19 15:35:18 -0700

    Register and login routes are done

--- api/resources/users.py
+++ api/resources/users.py
@@ -20,4 +20,31 @@ def register():
         return jsonify(data={}, status={'code': 400, 'message': 'A user with that email already exists'})
     except models.DoesNotExist:
         payload['password'] = generate_password_hash(payload['password'])
-        new_user = models.User.create[user=new_user]
+        new_user = models.User.create(**payload)
+
+        login_user(new_user)
+
+        user_dict = model_to_dict(new_user)
+        print(user_dict)
+        print(type(user_dict))
+
+        del user_dict['password']
+        return jsonify(data=user_dict, status={'code': 201, 'message': 'User created'})
+
+
+@user.route('/login', methods='POST')
+def login():
+    payload = request.get_json()
+    try:
+        user = models.User.get(models.User.email **payload['email'])
+        user_dict = model_to_dict(user)
+
+        if (check_password_hash(user_dict['password'], payload['password']))
+            del user_dict['password']
+            login_user(user)
+            print('user is: ', user)
+            return jsonify(data=user_dict, status={'code': 200, 'message': 'User authenticated'})
+        return jsonify(data={}, status={'code': 401, 'message': "Email or password is incorrect"})
+
+    except models.DoesNotExist:
+        return jsonify(data={}, status={'code': 401, 'message': "Email or password is incorrect"})
