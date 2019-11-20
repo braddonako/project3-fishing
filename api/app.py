3471ebb4609b9e05bc3b1e5f656d83767a579198
Brad Donakowski <braddonako@gmail.com>
2019-11-20 09:05:41 -0700

    minor changes

--- api/app.py
+++ api/app.py
@@ -1,14 +1,26 @@
 import models
-from flask import Flask, g
+from flask import Flask, g, request, jsonify
 from flask_cors import CORS
-
+from flask_login import LoginManager
 from resources.posts import post
 from resources.users import user
+from playhouse.shortcuts import model_to_dict
 
 DEBUG = True
 PORT = 8000
 
 app = Flask(__name__)
+app.secret_key = 'fjkdsaljfaldksjdsthisisoursecretkeyhahahjkfdhsa'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        return models.User.get(models.User.id == user_id)
+    except models.DoesNotExist:
+        return None
 
 @app.before_request
 def before_request():
